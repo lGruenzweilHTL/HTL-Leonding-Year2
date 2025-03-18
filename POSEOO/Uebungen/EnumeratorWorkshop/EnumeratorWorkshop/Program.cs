6da8ad0ad7079f1278058a5ace7ca590ff755c0f
luckylukii <lukas.gruenzweil@liwest.at>
2025-03-18 17:51:49 +0100

    POSEOO Enumerator Workshop

---
+++ POSEOO/Uebungen/EnumeratorWorkshop/EnumeratorWorkshop/Program.cs
@@ -0,0 +1,19 @@
+﻿using EnumeratorWorkshop;
+
+var a = new MyClass();
+
+int x = ((IComparable<int>)a).CompareTo(1);
+int y = a.CompareTo(a);
+    
+class MyClass : IComparable<int>, IComparable<MyClass>
+{ 
+    int IComparable<int>.CompareTo(int other)
+    {
+        return 1;
+    }
+
+    public int CompareTo(MyClass? other)
+    {
+        return 0;
+    }
+}
---
+++ POSEOO/Uebungen/EnumeratorWorkshop/EnumeratorWorkshop/SortedList.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace EnumeratorWorkshop;
+
+public sealed class SortedList<T> : IEnumerable<T>
+    where T : IComparable<T>
+{
+    private Node? _head; 
+
+    private sealed class Node 
+    {
+        public Node(T data) 
+        {
+            Data = data;
+        }
+
+        public T Data { get; } 
+        public Node? Next { get; set; } 
+    }
+    
+    public void Add(T data)
+    {
+        if (_head is null) 
+        {
+            _head = new Node(data);
+
+            return;
+        }
+
+        if (data.CompareTo(_head.Data) < 0) 
+        {
+            _head = new Node(data)
+            {
+                Next = _head
+            };
+
+            return;
+        }
+
+        var current = _head;
+        while (current.Next is not null
+               && data.CompareTo(current.Next.Data) > 0) 
+        {
+            current = current.Next;
+        }
+
+        current.Next = new Node(data) { Next = current.Next }; 
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node? current = _head;
+        while (current != null)
+        {
+            yield return current.Data;
+            current = current.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
