73f61be45172a50720ef4dcf9ba8813c2fb9cc94
lGruenzweilHTL <lukas.gruenzweil@liwest.at>
2025-03-27 10:16:34 +0100

    POSEPR LinkedList2 lookup function

--- POSEPR/Uebungen/LinkedList2/main.c
+++ POSEPR/Uebungen/LinkedList2/main.c
@@ -35,13 +35,37 @@ void deleteStart() {
     }
 }
 
+int lookup(int value) {
+    int idx = -1;
+    struct Node *curr = head;
+
+    while (curr != NULL) {
+        idx++;
+        if (curr->Data == value) {
+            return idx;
+        }
+
+        curr = curr->Next;
+    }
+
+    return -1;
+}
+
 int main(void) {
     head = NULL;
 
-    insertStart(5);
+    insertStart(7);
+    insertStart(9);
     insertStart(3);
 
     deleteStart();
 
     printList();
+
+    insertStart(5);
+    insertStart(8);
+    insertStart(15);
+
+    printf("7 at index: %d\n", lookup(7));
+    printf("8 at index: %d\n", lookup(8));
 }
